da2391402f91be1526f77d6cc15b9a1eb0070966
Kevin <133233113+kevinmlisboa@users.noreply.github.com>
2024-02-21 12:59:19 +0800

    Update PaymentApp.java

--- PaymentApp.java
+++ PaymentApp.java
@@ -1,27 +1,24 @@
 public class PaymentApp {
     public static void main(String[] args){
-        Order gcash = new Order("Keyboard", 10, 300.0, new Gcash());
-        System.out.println("Order item is " + gcash.getItem() + ".");
-        System.out.println("Unit price is " + gcash.getUnitPrice());
-        System.out.println("Quantity is " + gcash.getQuantity());
+        Order app = new Order("Keyboard", 10, 300.0, new Gcash());
+        System.out.println("Order item is " + app.getItem() + ".");
+        System.out.println("Unit price is " + app.getUnitPrice());
+        System.out.println("Quantity is " + app.getQuantity());
 
-        System.out.println("\nPayment order details if " + gcash.getMode().getClass().getSimpleName());
-        System.out.println("Discount Rate is " + gcash.getMode().determineDiscountRate());
-        System.out.println("Payment amount is " + gcash.getTotalAmount());
+        System.out.println("\nPayment order details if " + app.getMode().getClass().getSimpleName());
+        System.out.println("Discount Rate is " + app.getMode().determineDiscountRate());
+        System.out.println("Payment amount is " + app.getTotalAmount());
 
-        Order payMaya = new Order();
-        payMaya.setItem("Keyboard");
-        payMaya.setUnitPrice(300.0);
-        payMaya.setQuantity(10);
-        payMaya.setMode(new Maya());
-        payMaya.setTotalAmount();
-        System.out.println("\nPayment order details if " + payMaya.getMode().getClass().getSimpleName());
-        System.out.println("Discount Rate is " + payMaya.getMode().determineDiscountRate());
-        System.out.println("Payment amount is " + payMaya.getTotalAmount());
+        app.setMode(new Maya());
+        app.setTotalAmount();
+        System.out.println("\nPayment order details if " + app.getMode().getClass().getSimpleName());
+        System.out.println("Discount Rate is " + app.getMode().determineDiscountRate());
+        System.out.println("Payment amount is " + app.getTotalAmount());
 
-        Order shopeePay = new Order("Keyboard", 10, 300.0, new ShopeePay());
-        System.out.println("\nPayment order details if " + shopeePay.getMode().getClass().getSimpleName());
-        System.out.println("Discount Rate is " + shopeePay.getMode().determineDiscountRate());
-        System.out.println("Payment amount is " + shopeePay.getTotalAmount());
+        app.setMode(new ShopeePay());
+        app.setTotalAmount();
+        System.out.println("\nPayment order details if " + app.getMode().getClass().getSimpleName());
+        System.out.println("Discount Rate is " + app.getMode().determineDiscountRate());
+        System.out.println("Payment amount is " + app.getTotalAmount());
     }
 }
